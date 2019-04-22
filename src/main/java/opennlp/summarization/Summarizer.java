310976e22296de81cc0d8a0e70201c0d704cb355
dduuu <dishani2k14@gmail.com>
2019-04-22 12:56:39 +0530

    test jenkins code

--- src/main/java/opennlp/summarization/Summarizer.java
+++
@@ -1,4 +0,0 @@
-package opennlp.summarization;
-public interface  Summarizer {
-    public String summarize(String article, DocProcessor dp, int maxWords);
-}
--- src/main/java/opennlp/summarization/preprocess/WordWeight.java
+++
@@ -1,6 +0,0 @@
-package opennlp.summarization.preprocess;
-
-public interface WordWeight
-{
-    public double getWordWeight(String s);
-}
